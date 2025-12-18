1ee979c89f9b9d48a2b25251f164c75f13683787
ozennselin <129550025+ozennselin@users.noreply.github.com>
2025-12-18 22:07:16 +0300

    kategori listeleme yapıldı

---
+++ UrunTakip/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrunTakip.Data;
+
+namespace UrunTakip.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly NorthwindDB _db;
+
+        public CategoriesController(NorthwindDB db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var liste = _db.Categories.ToList();
+            return View(liste);
+        }
+    }
+}
--- UrunTakip/Data/Entities/Categories.cs
+++ UrunTakip/Data/Entities/Categories.cs
@@ -6,8 +6,8 @@ namespace UrunTakip.Data.Entities
     {
         [Key]
         public int CategoryId { get; set; }
-        public string CategoryName { get; set; }= string.Empty;
-        public string Description { get; set; }=string.Empty;
+        public string? CategoryName { get; set; }
+        public string? Description { get; set; }
         //public string Picture { get; set; }
     }
 }
