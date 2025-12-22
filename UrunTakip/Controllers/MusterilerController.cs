ced8f25c9c2bab474fe167f048a703b8a93ff113
ozennselin <129550025+ozennselin@users.noreply.github.com>
2025-12-22 22:27:33 +0300

    ProductInser işlemleri

---
+++ UrunTakip/Controllers/MusterilerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrunTakip.Data;
+
+namespace UrunTakip.Controllers
+{
+    public class MusterilerController : Controller
+    {
+        private readonly NorthwindDB _db;
+
+        public MusterilerController(NorthwindDB db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var liste = _db.Customers?.ToList();
+            return View(liste);
+        }
+    }
+}
