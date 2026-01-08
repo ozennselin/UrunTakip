7a9fedfa905cd42a60f50c1666a5d469ae5ae037
resulturfent <rsltrfnt@gmail.com>
2026-01-08 17:34:38 +0300

    mapping için giriş

---
+++ UrunTakip/DTO/ProductDTOS/ProductInsertDto.cs
@@ -0,0 +1,16 @@
+﻿namespace UrunTakip.DTO.ProductDTOS;
+
+public class ProductInsertDto
+{
+    public string ProductName { get; set; }
+    public int? SupplierId { get; set; }
+    public string SupplierName { get; set; }
+    public int? CategoryID { get; set; }
+    public string CategoryName { get; set; }
+    public string QuantityPerUnit { get; set; }
+    public decimal? UnitPrice { get; set; }
+    public short? UnitsInStock { get; set; }
+    public short? UnitsOnOrder { get; set; }
+    public short? ReorderLevel { get; set; }
+    public bool Discontinued { get; set; }
+}
