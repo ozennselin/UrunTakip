0699350e38f592e10bcbc9ae99f97481f290e665
ozennselin <129550025+ozennselin@users.noreply.github.com>
2026-01-13 07:16:46 +0300

    customer crud işlemleri yapıldı

--- UrunTakip/Controllers/MusterilerController.cs
+++ UrunTakip/Controllers/MusterilerController.cs
@@ -1,21 +1,85 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UrunTakip.Data;
+using UrunTakip.Data.Entities;
+using UrunTakip.DTO.CustomerDTOS;
+using UrunTakip.Mapping;
 
 namespace UrunTakip.Controllers
 {
     public class MusterilerController : Controller
     {
         private readonly NorthwindDB _db;
+        private readonly CustomerMapping _mapping;
 
         public MusterilerController(NorthwindDB db)
         {
             _db = db;
+            _mapping = new CustomerMapping(db);
         }
 
+        
         public IActionResult Index()
         {
-            var liste = _db.Customers?.ToList();
-            return View(liste);
+            var liste = _db.Customers.ToList();
+            List<CustomerListDto>dtoListe=new List<CustomerListDto>();
+            foreach(var item  in liste)
+            {
+                dtoListe.Add(_mapping.ListMapping(item));
+            }
+            return View(dtoListe);
+        }
+
+      
+        [HttpGet]
+        public IActionResult CustomerEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CustomerEkle(CustomerInsertDto dto)
+        {
+            Customers entity = _mapping.InsertDtoToEntity(dto);
+            _db.Customers.Add(entity);
+            _db.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CustomerGuncelle(string id)
+        {
+            var customer = _db.Customers.FirstOrDefault(x => x.CustomerID == id);
+            var dto = _mapping.UpdateMapping(customer);
+            return View(dto);
+        }
+
+        [HttpPost]
+        public IActionResult CustomerGuncelle(CustomerUpdateDto dto)
+        {
+            var customer = _db.Customers.FirstOrDefault(x => x.CustomerID == dto.CustomerID);
+
+            customer.CompanyName = dto.CompanyName;
+            customer.ContactName = dto.ContactName;
+            customer.City = dto.City;
+
+            _db.SaveChanges();
+            return View(dto);
+        }
+
+        [HttpGet]
+        public IActionResult CustomerSil(string id)
+        {
+            var customer = _db.Customers.FirstOrDefault(x => x.CustomerID == id);
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult CustomerSil(Customers customer)
+        {
+            var silinecek = _db.Customers.FirstOrDefault(x => x.CustomerID == customer.CustomerID);
+            _db.Customers.Remove(silinecek);
+            _db.SaveChanges();
+            return View();
         }
     }
 }
---
+++ UrunTakip/Mapping/CustomerMapping.cs
@@ -0,0 +1,70 @@
+﻿using UrunTakip.Data;
+using UrunTakip.Data.Entities;
+using UrunTakip.DTO.CustomerDTOS;
+
+namespace UrunTakip.Mapping
+{
+    public class CustomerMapping
+    {
+        NorthwindDB _db;
+
+        public CustomerMapping(NorthwindDB db)
+        {
+            _db = db;
+        }
+
+       
+        public CustomerListDto ListMapping(Customers customer)
+        {
+            CustomerListDto dto = new CustomerListDto();
+            dto.CustomerID = customer.CustomerID;
+            dto.CompanyName = customer.CompanyName;
+            dto.ContactName = customer.ContactName;
+            dto.ContactTitle = customer.ContactTitle;
+            dto.Address = customer.Address;
+            dto.City = customer.City;
+            dto.Region = customer.Region;
+            dto.PostalCode = customer.PostalCode;
+            dto.Country = customer.Country;
+            dto.Phone = customer.Phone;
+            dto.Fax = customer.Fax;
+            return dto;
+        }
+
+    
+        public CustomerUpdateDto UpdateMapping(Customers customer)
+        {
+            CustomerUpdateDto dto = new CustomerUpdateDto();
+            dto.CustomerID = customer.CustomerID;
+            dto.CompanyName = customer.CompanyName;
+            dto.ContactName = customer.ContactName;
+            dto.ContactTitle = customer.ContactTitle;
+            dto.Address = customer.Address;
+            dto.City = customer.City;
+            dto.Region = customer.Region;
+            dto.PostalCode = customer.PostalCode;
+            dto.Country = customer.Country;
+            dto.Phone = customer.Phone;
+            dto.Fax = customer.Fax;
+            return dto;
+        }
+
+       
+        public Customers InsertDtoToEntity(CustomerInsertDto dto)
+        {
+            Customers entity = new Customers();
+            entity.CustomerID = dto.CustomerID;
+            entity.CompanyName = dto.CompanyName;
+            entity.ContactName = dto.ContactName;
+            entity.ContactTitle = dto.ContactTitle;
+            entity.Address = dto.Address;
+            entity.City = dto.City;
+            entity.Region = dto.Region;
+            entity.PostalCode = dto.PostalCode;
+            entity.Country = dto.Country;
+            entity.Phone = dto.Phone;
+            entity.Fax = dto.Fax;
+            return entity;
+        }
+    }
+}
